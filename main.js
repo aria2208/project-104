685ef12b0dda42619feebf2496dacc6b8b224698
aria2208 <82940114+aria2208@users.noreply.github.com>
2021-06-26 12:47:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+Webcam.set({
+    Width: 350 ,
+    height: 300 ,
+    image_format: 'png',
+    png_quality: 90
+ });
+ 
+ Webcam.attach("camera");
+ 
+ function take_snapshot(){
+     Webcam.snap(function(data_uri){
+      document.getElementById("result").innerHTML='<img id="result_img" src="'+ data_uri +'"/>'
+     });
+ }
+ 
+ console.log('ml5 version: ', ml5.version);
+ 
+ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/model.json', modelLoaded);
+ 
+ function modelLoaded(){
+     console.log("modelLoaded");
+ }
